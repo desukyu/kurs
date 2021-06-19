43e4b5fac9d2fa56bd8a6341b9e9e4f3f406ab53
desukyu <desukyu@gmail.com>
2021-06-19 12:08:23 +0700

    Init quarkus

---
+++ src/test/java/org/kabanyo/kurs/KursBIResourceTest.java
@@ -0,0 +1,21 @@
+package org.kabanyo.kurs;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class KursBIResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/hello")
+          .then()
+             .statusCode(200)
+             .body(is("Hello RESTEasy"));
+    }
+
+}
---
+++ src/test/java/org/kabanyo/kurs/NativeKursBIResourceIT.java
@@ -0,0 +1,9 @@
+package org.kabanyo.kurs;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeKursBIResourceIT extends KursBIResourceTest {
+
+    // Execute the same tests but in native mode.
+}
